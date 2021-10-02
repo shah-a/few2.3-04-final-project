5143d2159008afd21da8091313bbfb14778fb2d6
Ali Shah <s.ashah@outlook.com>
2021-10-02 04:17:21 -0400

    Add: `smart-gophers.png` asset

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import smartGophers from '../../assets/smart-gophers.png';
+
+const Footer = () => (
+  <div className="Footer">
+    <img src={smartGophers} alt="" />
+  </div>
+);
+
+export default Footer;
