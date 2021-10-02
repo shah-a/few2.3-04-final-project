1ed32b7f4b6bb7cd4ea189bd04bdde5451296c0d
Ali Shah <s.ashah@outlook.com>
2021-10-02 04:18:14 -0400

    Add: component placeholders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
+import Footer from './Footer';
+
 const App = () => (
   <div className="App min-h-screen flex flex-col bg-gradient-to-r from-green-200 to-green-400">
     <h1 className="mx-auto mt-52 text-gray-800">Salaam, world! 😊</h1>
+    <Footer />
   </div>
 );
 
