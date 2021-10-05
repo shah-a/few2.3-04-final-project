61e23aa6913679cda9c6808976fb190b0cf95145
Ali Shah <s.ashah@outlook.com>
2021-10-05 04:09:54 -0400

    Add: `Array` component

---
+++ src/components/ArrayList/Array/index.jsx
@@ -0,0 +1,15 @@
+const Array = () => (
+  <div className="Array">
+    <label className="array-label">
+      <h2>Array #1</h2>
+      <textarea
+        rows="7"
+        placeholder="E.g: 39, 71, 37, 87, 69, 48, 49, 45, 85, 63, 23, 13, 26, 76, 56, 99, 17, 1, 81, 25, 18, 15, 20, 68, 31..."
+        value=""
+        className="array-textarea"
+      />
+    </label>
+  </div>
+);
+
+export default Array;
--- src/components/ArrayList/index.jsx
+++ src/components/ArrayList/index.jsx
@@ -1,17 +1,12 @@
+import Array from './Array';
+
 const ArrayList = () => (
-  <section className="ArrayList">
-    <div id="array-1-card" className="array-card">
-      <label htmlFor="array-1" className="array-label">
-        <h2>Array #1</h2>
-        <textarea id="array-1" className="array-textarea" rows="7" />
-      </label>
-    </div>
-    <div id="array-2-card" className="array-card">
-      <label htmlFor="array-2" className="array-label">
-        <h2>Array #2</h2>
-        <textarea id="array-2" className="array-textarea" rows="7" />
-      </label>
-    </div>
+  <section className="ArrayList w-full outline">
+    <Array />
+    <Array />
+    <Array />
+    <Array />
+    <Array />
   </section>
 );
 
