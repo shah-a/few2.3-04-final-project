02050efabb63dda2f61c4734868f06324aee599d
Ali Shah <s.ashah@outlook.com>
2021-10-05 07:32:03 -0400

    Add: styled algorithm dropdowns
    NOTES:
    - Styling is finished; next phase is to add button functionality

--- src/components/ConfigList/AlgoSelectList/AlgoSelect/index.jsx
+++ src/components/ConfigList/AlgoSelectList/AlgoSelect/index.jsx
@@ -1,11 +1,13 @@
 const AlgoSelect = () => (
   <div className="AlgoSelect">
-    <label htmlFor="algorithm-1" className="algorithm-label">Array #1: </label>
-    <select id="algorithm-1" className="algorithm-select">
-      <option value="bubble">Bubble Sort</option>
-      <option value="selection">Selection Sort</option>
-      <option value="insertion">Insertion Sort</option>
-    </select>
+    <label className="space-x-2">
+      <span>Array #1:</span>
+      <select className="p-2 text-black rounded-sm">
+        <option value="bubble">Bubble Sort</option>
+        <option value="selection">Selection Sort</option>
+        <option value="insertion">Insertion Sort</option>
+      </select>
+    </label>
   </div>
 );
 
--- src/components/ConfigList/AlgoSelectList/index.jsx
+++ src/components/ConfigList/AlgoSelectList/index.jsx
@@ -3,11 +3,13 @@ import AlgoSelect from './AlgoSelect';
 const AlgoSelectList = () => (
   <div className="AlgoSelect outline">
     <h3>Which Sorting Algorithms?</h3>
-    <AlgoSelect />
-    <AlgoSelect />
-    <AlgoSelect />
-    <AlgoSelect />
-    <AlgoSelect />
+    <div className="space-y-2">
+      <AlgoSelect />
+      <AlgoSelect />
+      <AlgoSelect />
+      <AlgoSelect />
+      <AlgoSelect />
+    </div>
   </div>
 );
 
