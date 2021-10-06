93fb5594aaecc65cc57d56ec165c3ca17477da9a
Ali Shah <s.ashah@outlook.com>
2021-10-06 01:49:38 -0400

    Add: console message on start button click
    NOTES:
    - Backend is not yet developed. Clicking the start button will
      trigger a notification for this in the console, as well as the
      current state of the arrays.

--- src/components/ConfigList/index.jsx
+++ src/components/ConfigList/index.jsx
@@ -1,19 +1,34 @@
+import { useSelector } from 'react-redux';
 import ArrayCount from './ArrayCount';
 import ArrayInput from './ArrayInput';
 import AlgoSelectList from './AlgoSelectList';
 
-const ConfigList = () => (
-  <section className="ConfigList w-full space-y-3 text-center">
-    <div className="outline">
-      <h2>Configuration</h2>
-    </div>
-    <ArrayCount />
-    <ArrayInput />
-    <AlgoSelectList />
-    <div className="outline">
-      <button type="button" id="start-button" className="config-button">Start</button>
-    </div>
-  </section>
-);
+const ConfigList = () => {
+  const arrays = useSelector((state) => state.arrays);
+
+  return (
+    <section className="ConfigList w-full space-y-3 text-center">
+      <div className="outline">
+        <h2>Configuration</h2>
+      </div>
+      <ArrayCount />
+      <ArrayInput />
+      <AlgoSelectList />
+      <div className="outline">
+        <button
+          type="button"
+          id="start-button"
+          className="config-button"
+          onClick={() => {
+            console.log('Backend not yet implemented!');
+            console.log(arrays);
+          }}
+        >
+          Start
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default ConfigList;
